504f2052b0108d3465190e2339726a46437687d8
Divine Idehen <idehendivine16@gmail.com>
2026-07-21 14:11:38 +0100

    feat: Add configuration files for Scribe API documentation and service-repository structure

---
+++ config/service-repository.php
@@ -0,0 +1,43 @@
+<?php
+
+return [
+    /**
+     * The directory for all the repositories
+     */
+    'repository_directory' => 'app/Repositories',
+
+    /**
+     * Default repository namespace
+     */
+    'repository_namespace' => "App\Repositories",
+
+    /**
+     * The directory for all the services
+     */
+    'service_directory' => 'app/Services',
+
+    /**
+     * Default service namespace
+     */
+    'service_namespace' => "App\Services",
+
+    /**
+     * Repository interface name suffix
+     */
+    'repository_interface_suffix' => 'Repository',
+
+    /**
+     * Repository name suffix
+     */
+    'repository_suffix' => 'RepositoryImplement',
+
+    /**
+     * Service interface name suffix
+     */
+    'service_interface_suffix' => 'Service',
+
+    /**
+     * Service name suffix
+     */
+    'service_suffix' => 'ServiceImplement',
+];
