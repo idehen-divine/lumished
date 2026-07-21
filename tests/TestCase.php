cbd6633a158acb7035532a0f766e5782a143c85e
Divine Idehen <idehendivine16@gmail.com>
2026-07-21 22:07:20 +0100

    test: configure testing environment with in-memory SQLite

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,26 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        $env = [
+            'APP_ENV' => 'testing',
+            'DB_CONNECTION' => 'sqlite',
+            'DB_DATABASE' => ':memory:',
+            'MAIL_MAILER' => 'array',
+            'CACHE_STORE' => 'array',
+            'QUEUE_CONNECTION' => 'sync',
+            'SESSION_DRIVER' => 'array',
+            'BCRYPT_ROUNDS' => '4',
+            'AUTH_THROTTLE_LIMIT' => '100',
+        ];
+
+        foreach ($env as $key => $value) {
+            $_ENV[$key] = $value;
+            $_SERVER[$key] = $value;
+            putenv("$key=$value");
+        }
+
+        parent::setUp();
+    }
 }
