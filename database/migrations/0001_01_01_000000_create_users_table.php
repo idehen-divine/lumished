5c6e196c42e57345f832a2192d774ff4197ce3a5
Divine Idehen <idehendivine16@gmail.com>
2026-07-21 22:06:23 +0100

    feat: update users table schema for auth system

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,11 +12,19 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->uuid('id')->primary();
+            $table->string('first_name');
+            $table->string('last_name')->nullable();
+            $table->string('other_name')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone_no')->nullable();
+            $table->string('profile_image')->nullable();
+            $table->string('gender')->nullable();
+            $table->string('status')->default('ACTIVE');
+            $table->text('two_factor_secret')->nullable();
+            $table->timestamp('two_factor_confirmed_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
