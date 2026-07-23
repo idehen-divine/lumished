c311769030ec8da66612430ac278e08c3beaf5df
Divine Idehen <idehendivine16@gmail.com>
2026-07-23 16:30:54 +0100

    feat: add ProductResource

---
+++ app/Http/Resources/ProductResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'store_id' => $this->store_id,
+            'name' => $this->name,
+            'description' => $this->description,
+            'price' => $this->price,
+            'compare_at_price' => $this->compare_at_price,
+            'stock_quantity' => $this->stock_quantity,
+            'photo' => $this->photo,
+            'photos' => $this->photos,
+            'status' => $this->status?->name,
+            'categories' => CategoryResource::collection($this->whenLoaded('categories')),
+            'store' => new PublicStoreResource($this->whenLoaded('store')),
+            'created_at' => $this->created_at?->format('Y-m-d H:i:s'),
+        ];
+    }
+}
