64b29dc082bb4ca2ef4d08c39a35ea6e3f8322b4
Divine Idehen <idehendivine16@gmail.com>
2026-07-23 16:31:27 +0100

    feat: define public store routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,18 @@
 <?php
 
+use App\Http\Controllers\Api\PublicStoreController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+/**********************    Public Store Routes    ***********************/
+Route::prefix('v1')->middleware('throttle:60,1')->controller(PublicStoreController::class)->group(function () {
+    Route::get('/stores', 'index')->name('public.stores.index');
+    Route::get('/stores/{slug}', 'show')->name('public.stores.show');
+    Route::get('/stores/{slug}/products', 'products')->name('public.stores.products');
+    Route::get('/stores/{slug}/categories', 'categories')->name('public.stores.categories');
+    Route::get('/products/{id}', 'showProduct')->name('public.products.show');
+});
