d4f9b86fba3e9c7a48436896c2352f512b0d9d6e
Divine Idehen <idehendivine16@gmail.com>
2026-07-30 23:00:48 +0100

    refactor: remove unused user route and request import from API routes

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\StoreController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
 /**********************    Public Store Routes    ***********************/
 Route::prefix('v1')->middleware('throttle:60,1')->controller(StoreController::class)->group(function () {
